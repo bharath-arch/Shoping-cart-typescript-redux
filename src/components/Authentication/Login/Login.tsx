44a36103e685de329dced5c03d121c92679dcec2
unknown <bharathkumar100q@gmail.com>
2024-08-16 02:18:34 -0700

    cart

--- src/components/Authentication/Login/Login.tsx
+++ src/components/Authentication/Login/Login.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { LoginBtn } from "../../ButtonGroup/LoginBtn/LoginBtn";
 
 export const Login: React.FC = () => {
   const navigate = useNavigate();
@@ -66,12 +67,7 @@ export const Login: React.FC = () => {
               />
              </div>
               <div className="flex justify-center items-center">
-                <button
-                  type="submit"
-                  className="mt-3 text-center border-2 rounded-lg w-[22rem] p-2 font-arima bg-blue-600 text-white text-xl items-center hover:bg-blue-700 transition-transform duration-300 ease-in-out transform hover:scale-95"
-                >
-                  Continue
-                </button>
+                <LoginBtn/>
               </div>
             </form>
 
---
+++ src/components/ButtonGroup/LoginBtn/LoginBtn.tsx
@@ -0,0 +1,12 @@
+export const LoginBtn: React.FC = () => {
+  return (
+    <>
+      <button
+        type="submit"
+        className="mt-3 text-center border-2 rounded-lg w-[22rem] p-2 font-arima bg-blue-600 text-white text-xl items-center hover:bg-blue-700 transition-transform duration-300 ease-in-out transform hover:scale-95"
+      >
+        Continue
+      </button>
+    </>
+  );
+};
