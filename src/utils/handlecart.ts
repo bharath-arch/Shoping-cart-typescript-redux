bad7e4563a8fb044a79451f5d90a6d011f87d25d
unknown <bharathkumar100q@gmail.com>
2024-08-20 09:42:22 -0700

    cart

--- src/utils/handlecart.ts
+++ src/utils/handlecart.ts
@@ -18,10 +18,9 @@ export const useHandleAddCart = (id: number, name: string, image: string | undef
   
 };
 
-// Handle removing items from the cart
+
 export const useHandleRemoveCart = (id: number, name: string, image: string | undefined ) => {
   const dispatch = useDispatch();
-
   const handleRemoveCart = () => {
     const cartItem: CartItem = { id, name, quantity: 1, image };
     dispatch(removefromcart(cartItem));
